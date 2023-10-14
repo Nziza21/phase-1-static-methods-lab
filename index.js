64d322fbb5e2e66d76d140036c8ff1639dcf5e99
Nziza21 <144722415+Nziza21@users.noreply.github.com>
2023-10-14 17:39:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-'\s]/g, '');
+  }
+
+  static titleize(string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = string.split(' ');
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+
+    return this.capitalize(titleizedWords.join(' '));
+  }
 }
+console.log(Formatter.capitalize('hello world'));
+console.log(Formatter.sanitize("hello-world's 1 this, is: it!"));
+console.log(Formatter.titleize('the brown dog and cat are playing in the yard')); 
